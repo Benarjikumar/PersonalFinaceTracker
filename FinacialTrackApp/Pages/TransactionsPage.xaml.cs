5c51b92e8d9679c54cef5cda5099591dc8ab6833
Tummallapalli Benarji <benarjikumar123@gmail.com>
2025-04-03 11:23:09 +0530

    Ui Updated

--- FinacialTrackApp/Pages/TransactionsPage.xaml.cs
+++ FinacialTrackApp/Pages/TransactionsPage.xaml.cs
@@ -1,3 +1,5 @@
+using FinacialTrackApp.ViewModel;
+
 namespace FinacialTrackApp.Pages;
 
 public partial class TransactionsPage : ContentPage
@@ -5,5 +7,13 @@ public partial class TransactionsPage : ContentPage
 	public TransactionsPage()
 	{
 		InitializeComponent();
-	}
+
+        BindingContext = new TransactionViewModel();
+
+    }
+
+    private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
 }
